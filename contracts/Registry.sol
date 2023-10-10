192ab566698a7d01802229ff3090f6c9c567fc94
gabrielstoica <marius_gabriel1998@yahoo.com>
2023-10-10 21:29:39 +0300

    feat(Registry): create method to retrieve a did based on a signature

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -25,6 +25,7 @@ contract Registry is IRegistry {
 
   error Unauthorized();
   error InvalidOwnerAddress();
+  error NotAnAttendee();
 
   /*//////////////////////////////////////////////////////////////////////////
                                 CONSTRUCTOR
@@ -86,6 +87,18 @@ contract Registry is IRegistry {
     return owner;
   }
 
+  function getIdentity(bytes memory signature) external view returns (string memory) {
+    bytes32 message = craftMessage();
+    address attendee = recoverSigner(message, signature);
+
+    Identity memory attendeeIdentity = identity[attendee];
+    string memory did = attendeeIdentity.did;
+
+    if (bytes(did).length == 0) revert NotAnAttendee();
+
+    return did;
+  }
+
   /*//////////////////////////////////////////////////////////////////////////
                                 ADMIN METHODS
   //////////////////////////////////////////////////////////////////////////*/
@@ -99,5 +112,48 @@ contract Registry is IRegistry {
     address oldOwner = owner;
     owner = newOwner;
     emit TransferOwnership(oldOwner, newOwner);
+  }
+
+  /*//////////////////////////////////////////////////////////////////////////
+                                     UTILS
+    //////////////////////////////////////////////////////////////////////////*/
+
+  /// @notice Splits a signature into its constituent `v`, `r`, and `s` components
+  /// @param sig The signature bytes to be split
+  /// @return v The recovery byte of the signature
+  /// @return r The `r` component of the signature
+  /// @return s The `s` component of the signature
+  function splitSignature(bytes memory sig) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
+    require(sig.length == 65);
+
+    assembly {
+      // first 32 bytes, after the length prefix
+      r := mload(add(sig, 32))
+      // second 32 bytes
+      s := mload(add(sig, 64))
+      // final byte (first byte of the next 32 bytes)
+      v := byte(0, mload(add(sig, 96)))
+    }
+
+    return (v, r, s);
+  }
+
+  /// @notice Recovers the address of the signer from the provided message and signature
+  /// @dev The hashed message must be prefixed with `\x19Ethereum Signed Message:\n32`
+  /// @param message The keccak256 hashed message that was signed
+  /// @param sig The signature bytes from which the signer's address will be recovered
+  /// @return The address of the signer that produced the provided signature
+  function recoverSigner(bytes32 message, bytes memory sig) internal pure returns (address) {
+    (uint8 v, bytes32 r, bytes32 s) = splitSignature(sig);
+
+    return ecrecover(message, v, r, s);
+  }
+
+  /// @notice Prefixes the provided hash with "\x19Ethereum Signed Message:\n" + len(message) and then hashes the result
+  /// @dev This is used to comply with the Ethereum's default signing scheme
+  /// @return The prefixed and hashed value
+  function craftMessage() internal pure returns (bytes32) {
+    bytes memory message = "Sign in with Ethereum wallet to verify your identity!";
+    return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n53", message));
   }
 }
